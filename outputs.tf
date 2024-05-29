4b5e9e6e1c63e0477b2d1d2c4b52751016757a65
kaminenileela <kaminenileela1992@email.com>
2024-05-29 19:04:01 -0400

    vpc

--- outputs.tf
+++ outputs.tf
@@ -22,6 +22,10 @@ output "aws_db_subnet_group_id" {
     value = aws_db_subnet_group.default.id
 }
 
+output "aws_db_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
+
 output "igw_id" {
     value = aws_internet_gateway.igw.id
 }
