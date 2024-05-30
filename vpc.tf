abec56b02d4ca09011f313bd3279bfe6923f7f25
kaminenileela <kaminenileela1992@email.com>
2024-05-30 17:28:23 -0400

    vpc

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr
+    instance_tenancy = "default"
     enable_dns_hostnames = var.dns_enable_hostnames
 
     tags = merge(var.common_tags,
